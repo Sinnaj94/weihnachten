9e992efadd5dc1abe71b61e5377e08b0aa377b0f
Jahr <JJahr@eventim.ag>
2016-10-24 14:16:58 +0200

    sql database

--- index.php
+++ index.php
@@ -1,14 +1,37 @@
 <!DOCTYPE html>
+<?php
+require_once('konfiguration.php');
+$db_link = mysqli_connect (
+    MYSQL_HOST, 
+    MYSQL_BENUTZER, 
+    MYSQL_KENNWORT, 
+    MYSQL_DATENBANK
+);
+
+if ( $db_link )
+{
+
+	$sql = "SELECT * FROM users";
+	$result = $db_link->query($sql);
+    if($result->num_rows >0){
+    	while($row = $result->fetch_assoc()){
+    	}
+    }
+
+}
+?>
 <html>
 <head>
-
+	<!--http://paletton.com/#uid=15C0u0kllllcz++gXw0pJaGu700-->
 	<title>Wichteln - Weihnachten 2016</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="styles/stylesheet.css">
 
-
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 
 	<!-- Latest compiled and minified CSS -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+	<link rel="stylesheet" type="text/css" href="styles/stylesheet.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
 	<!-- Optional theme -->
@@ -20,12 +43,19 @@
 	<script src="scripts/logic.js"></script>
 </head>
 <body>
-	<form class="form-signin">
-		<h2 class="form-signin-heading">Login</h2>
-		<label for="inputEmail" class="sr-only">Benutzername</label>
-		<input type="text" id="inputEmail" class="form-control" placeholder="Benutzername" required="" autofocus="">
-		<input type="password" id="inputPassword" class="form-control" placeholder="Password" required="" maxlength="4" pattern="[0-9]*">
-		<button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>
-	</form>
+	<div class="main-container">
+		<form class="form-signin">
+			<label for="inputEmail"></label>
+			<div class="inner-addon left-addon">
+				<i class="glyphicon glyphicon-user"></i>
+				<input type="text" class="form-control" placeholder="Benutzername" required=""/>
+			</div>
+			<div class="inner-addon left-addon">
+				<i class="glyphicon glyphicon-lock"></i>
+				<input type="password" id="inputPassword" class="form-control" placeholder="Password" required="" maxlength="4" pattern="[0-9]*">
+			</div>
+			<button class="btn weihnachtsbutton btn-lg btn-block " type="submit">Login</button>
+		</form>
+	</div>
 </body>
 </html>
